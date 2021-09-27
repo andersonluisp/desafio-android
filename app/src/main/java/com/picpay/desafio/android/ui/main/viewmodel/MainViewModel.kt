0ca17037c9f864502ce57e31b660d400a94135b5
Anderson Pimentel <andersonluiscp@gmail.com>
2021-09-27 12:39:26 -0300

    Refactoring the MainViewModel

--- app/src/main/java/com/picpay/desafio/android/ui/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/picpay/desafio/android/ui/main/viewmodel/MainViewModel.kt
@@ -1,20 +1,10 @@
 package com.picpay.desafio.android.ui.main.viewmodel
 
-import android.util.Log
-import androidx.lifecycle.*
-import com.picpay.desafio.android.data.model.User
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.picpay.desafio.android.data.repository.Repository
-import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainViewModel(private val repository: Repository) : ViewModel() {
-
-    private val mainViewState = MainViewState()
-    private val _state = MutableLiveData(mainViewState)
-    val state: LiveData<MainViewState>
-        get() = _state
 
     val users = repository.getUsers().asLiveData()
 }
